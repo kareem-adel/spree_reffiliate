c6b09469aab8a68774d4cb2dc62a6d5d7846dcb5
kareem-adel <kareem.adel.fathi@gmail.com>
2018-01-02 20:14:49 +0200

    remove refer UI

--- app/overrides/spree/users/show.rb
+++ app/overrides/spree/users/show.rb
@@ -1,9 +1,9 @@
-Deface::Override.new(
-  virtual_path: "spree/users/show",
-  name: "user_info",
-  insert_after: "[data-hook='account_summary'] #user-info dd:first",
-  partial: 'spree/users/referral_links'
-)
+#Deface::Override.new(
+#  virtual_path: "spree/users/show",
+#  name: "user_info",
+#  insert_after: "[data-hook='account_summary'] #user-info dd:first",
+#  partial: 'spree/users/referral_links'
+#)
 
 Deface::Override.new(
   virtual_path: 'spree/users/show',
